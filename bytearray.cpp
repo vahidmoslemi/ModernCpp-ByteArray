d08ac4f0eed0d1db0bbd95c8b57e312e06a5f409
Vahid Moslemi <6877068+vahidmoslemi@users.noreply.github.com>
2020-05-10 12:26:02 +0430

    initial commit

---
+++ bytearray.cpp
@@ -0,0 +1,115 @@
+#include "bytearray.h"
+#include <cstring>
+#include <memory>
+
+ByteArray::ByteArray()
+{
+    m_data_size = 0;
+    m_data = nullptr;
+}
+
+ByteArray::ByteArray(ByteArray& other)
+{
+    m_data_size = other.m_data_size;
+    m_data = new char [m_data_size];
+    std::copy(other.m_data,other.m_data+other.m_data_size,m_data);
+}
+
+ByteArray::ByteArray(ByteArray&& other)
+{
+    m_data_size = other.m_data_size;
+    m_data = other.m_data;
+    other.m_data = nullptr;
+    other.m_data_size = 0;
+}
+
+ByteArray::ByteArray(unsigned long t_data_size){
+    m_data_size = t_data_size;
+    m_data = new char[m_data_size];
+}
+
+ByteArray::ByteArray(const char* t_data) : m_data_size{strlen(t_data)}
+{
+    m_data = new char [m_data_size];
+    std::copy(t_data,t_data+m_data_size,m_data);
+}
+
+ByteArray::ByteArray(char* t_data,unsigned long t_data_size) : m_data_size{t_data_size}
+{
+    m_data = t_data;
+}
+
+ByteArray::~ByteArray()
+{
+    delete [] m_data;
+    m_data = nullptr;
+    m_data_size = 0;
+}
+
+
+//Copy Assignment Operator
+ByteArray& ByteArray::operator=(const ByteArray&  other)
+{
+    //avoid self copy
+    if(this!=&other)
+    {
+        delete[] m_data;
+        m_data=nullptr;
+        m_data_size = other.m_data_size;
+        m_data = new char[m_data_size];
+        std::copy(other.m_data,other.m_data + m_data_size,m_data);
+    }
+    return *this;
+}
+
+//Move Assignment Operator
+ByteArray& ByteArray::operator=(ByteArray&&  other)
+{
+    //avoid self copy
+    if(this!=&other)
+    {
+        delete[] m_data;
+        m_data_size = other.m_data_size;
+        m_data = other.m_data;
+        other.m_data = nullptr;
+        other.m_data_size = 0;
+    }
+    return *this;
+}
+
+void ByteArray::setData(const char* t_data, unsigned long t_data_size)
+{
+    delete[] m_data;
+    m_data_size = t_data_size;
+    m_data=new char[m_data_size];
+    std::copy(t_data,t_data + m_data_size, m_data);
+}
+
+void ByteArray::setData(char* t_data, unsigned long t_data_size)
+{
+    delete[] m_data;
+    m_data_size = t_data_size;
+    m_data = t_data;
+}
+
+std::vector<ByteArray> ByteArray::splite(char delimiter)
+{
+    std::vector<ByteArray> result;
+    unsigned long from=0;
+    for(unsigned long i=0; i<m_data_size;i++)
+    {
+        if(m_data[i]==delimiter)
+        {
+            if(i>from)
+                result.push_back(this->mid(from,i-from));
+
+            from = i+1;
+        }
+    }
+    //if delimiter not found
+    if(from==0)
+    {
+        result.push_back(ByteArray{this->data()});
+    }
+    return result;
+}
---
+++ bytearray.h
@@ -0,0 +1,106 @@
+/*
+ * Pure modern C++ ByteArray type implemented as a wrapper over char*
+ * Supports move semantics to provide good performance in daily usage
+ *
+ * Written in 2020 by Vahid Moslemi Vayeghan
+ *
+ * Hope this type help many developers in daily use projects to pass and
+ * return char* values in a safe and simple manner
+ */
+
+#ifndef BYTEARRAY_H
+#define BYTEARRAY_H
+
+#include<vector>
+
+class ByteArray
+{
+public:
+    //construct as empty, by data_length or by const value
+    ByteArray();
+    /**
+     * @brief ByteArray : copy constructor
+     */
+    ByteArray(ByteArray&);
+
+    /**
+     * @brief ByteArray : Move Constructor
+     */
+    ByteArray(ByteArray&&);
+    explicit ByteArray(unsigned long t_data_size);
+    ByteArray(const char* t_data);
+    explicit ByteArray(char* t_data,unsigned long t_data_size);
+    ~ByteArray();
+
+    /**
+     * @brief operator =  Copy Assignment Operator
+     * @param other
+     * @return
+     */
+    ByteArray& operator=(const ByteArray&  other);
+
+    /**
+     * @brief operator = Move Assignment Operator
+     * @param other
+     * @return
+     */
+    ByteArray& operator=(ByteArray&&  other);
+
+    /**
+     * @brief setData : copy data into the ByteArray
+     * @param t_data
+     * @param t_data_size
+     */
+    void setData(const char* t_data, unsigned long t_data_size);
+
+    /**
+     * @brief setData : move data into the ByteArray. data will be controlled by ByteArray from now on
+     * @param t_data
+     * @param t_data_size
+     */
+    void setData(char* t_data, unsigned long t_data_size);
+
+    /**
+     * @brief release : release data control and return data pointer to new holder
+     *                  it would be better to first getting data size using getSize methods
+     *                  before invoking release method
+     * @return
+     */
+    char* release(){
+        char* tmp = m_data;
+        m_data = nullptr;
+        m_data_size = 0;
+        return tmp;
+    }
+
+    /**
+     * @brief release : release data control and return data pointer to new holder
+     *                  it would be better to first getting data size using getSize methods
+     *                  before invoking release method
+     * @return
+     */
+    const char* data(){
+        return m_data;
+    }
+
+    /**
+     * @brief getSize : returns size of the char array which holds data
+     * @return
+     */
+    unsigned long getSize(){ return m_data_size;}
+
+    ByteArray mid(unsigned long from,unsigned long len)
+    {
+        ByteArray result(len);
+        std::copy(m_data+from,m_data+from+len,result.m_data);
+        return  result;
+    }
+
+    std::vector<ByteArray> splite(char delimiter);
+
+private:
+    unsigned long m_data_size;
+    char* m_data;
+};
+
+#endif // BYTEARRAY_H
